93b650f70a43ef8741235f57a116f5f434db002b
jmurrii <jonmurrii@gmail.com>
2020-05-14 14:37:09 +0000

    Bootstrap changes to NavBar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,7 @@ const randomQuote = document.getElementById('randomQuote')
 const userInput = document.getElementById('userInput');
 const gameTimer = document.getElementById('timer');
 
-
 let hardQuotes = [
-
     '“Life must be understood backward. But it must be lived forward” – Soren Kierkegaard',
     '“The greater the difficulty, the more glory in surmounting it” – Epicurus',
     '“That man is wisest who, like Socrates, realises that his wisdom is worthless” – Plato',
@@ -20,9 +18,7 @@ let hardQuotes = [
     '“How wonderful it is that nobody need wait a single moment before starting to improve the world.” ― Anne Frank',
 
 ]
-
 let easyQuotes = [
-
     'life must be understood backward but it must be lived forward',
     'the greater the difficulty the more glory in surmounting it',
     'that man is wisest who like socrates realises that his wisdom is worthless',
@@ -30,15 +26,20 @@ let easyQuotes = [
     'if you would be a real seeker after truth it is necessary that at least once in your life you doubt as far as possible all things',
     'freedom is secured not by the fulfilling of ones desires but by the removal of desire',
     'the brave man is he who overcomes not only his enemies but his pleasures',
+    'the unexamined life is not worth living',
+    'without music life would be a mistake',
+    'go to heaven for the climate and hell for the company',
+    'how wonderful it is that nobody need wait a single moment before starting to improve the world',
+];
+
+let quotesForCurrentRun = [
+
 ]
 
-
-// Free Code Camp Lesson
 function newQuote() {
     let randomNum = Math.floor(Math.random() * (easyQuotes.length));
     document.getElementById('randomQuote').innerHTML = easyQuotes[randomNum];
 }
-
 
 function handleKeyUp() {
     startTimer();
@@ -53,8 +54,7 @@ function startTimer() {
         timerRunning = true;
         var startTime = Date.now();
 
-// stack overflow
-        var interval = setInterval(function () {
+         interval = setInterval(function () {
             var elapsedTime = Date.now() - startTime;
             document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
         }, 100);
@@ -66,24 +66,43 @@ function myStopFunction() {
     timerRunning = false;
 }
 
-let quoteTracker = 0;
+// only two difficult problems
+// naming things, cache invalidation, and off-by-one errors
+
+let tracker = 0;
+
+ function spellCheck() {
+     // get the input from the text area (that the user has typed)
+     let userInputText = document.getElementById('userInput').value
+     // check user input against the current quote, to see if they're equal
+     let randomQuoteText = document.getElementById('randomQuote').innerText
+     // if they're equal, do something (for starters, alert "correct!")
+     if (userInputText === randomQuoteText) {
+         tracker++;
+         quotesForCurrentRun.push(userInputText);
+         console.log(quotesForCurrentRun);
+         if (tracker > 1) {
+            gameComplete();
+         }
+         newQuote();
+         document.getElementById('userInput').value = "";
+         // alert("Correct");
+     }
+ }
 
 
-function spellCheck() {
-    // get the input from the text area (that the user has typed)
-    let userInputText = document.getElementById('userInput').value
-    console.log(userInputText);
-    // check that input against the current quote, to see if they're equal
-    let randomQuoteText = document.getElementById('randomQuote').innerText
-    console.log(randomQuoteText);
-    // if they're equal, do something (for starters, alert "correct!")
-    if (userInputText === randomQuoteText) {
-        quoteTracker++;
-        if (quoteTracker > 1) {
-        newQuote();
-        document.getElementById('userInput').value = "";
-       // alert("Correct");
-    }
+function gameComplete() {
+     alert("Game Complete!!!!");
+     // current time = timer at gameComplete
+   const currentTime = document.getElementById('timer').innerText;
+    const numberOfWordsInTypedSentences = quotesForCurrentRun.reduce((accumulator, currentValue) => {
+        accumulator += currentValue.split(' ').length;
+        return accumulator;
+    }, 0);
+    let minutesElapsed = parseFloat(currentTime) / 60;
+    let wordsPerMinute = Math.round(numberOfWordsInTypedSentences / minutesElapsed);
+   alert(`Your Typing Speed is ${wordsPerMinute} words per minute`);
+   stopAndClearTimer();
 }
 
 function stopAndClearTimer() {
@@ -91,11 +110,6 @@ function stopAndClearTimer() {
     document.getElementById('timer').innerText = "00.00";
 }
 
-function gameComplete() {
-    alert("Game Complete!!!!");
-    //current time = timer at gameComplete
-    stopAndClearTimer();
-}
 
 
 
